0f899063a16aaa53e62c9145f49ca7ffa0812feb
Aymenfisher <fisheraymen@gmail.com>
2022-09-19 18:53:39 +0100

    added .readme , fixed some bugs

--- main.js
+++ main.js
@@ -2,25 +2,28 @@
 
 
 
-let dropdown = document.querySelectorAll('.dropdown'); // Dropdown div that contains the button and the menu
+let dropdown = document.querySelectorAll('.dropdown  button'); // selecting buttons
 
 
-
-function showDropdownMenu(menu){
+function showDropdownMenu(menu,iconParent){
     menu.style.display = 'grid';  // since we are using grid, you can modify it
+    iconParent.querySelector('#arrow-up').style.display = '';  // icons
+    iconParent.querySelector('#arrow-down').style.display = 'none';
 }
 
-function hideDropdownMenu(menu){
+function hideDropdownMenu(menu,iconParent){
     menu.style.display = 'none';
+    iconParent.querySelector('#arrow-up').style.display = 'none';  // icons
+    iconParent.querySelector('#arrow-down').style.display = '';
 }
 
 function showHideDropdownMenu (e){
     let menu = e.target.nextElementSibling // the dropdown menu is the next sibling element of the button
     let menuComputedDisplay = window.getComputedStyle(menu).display;  // getting the computed display since we set it with css stylesheet
     if (menuComputedDisplay == 'none'){
-        showDropdownMenu(menu);
+        showDropdownMenu(menu,e.target);
     } else{
-        hideDropdownMenu(menu);
+        hideDropdownMenu(menu,e.target);
     }
 }
 
